e61a1e51abd04de7955fa61751753f7c4c50a29f
langitdb <deepak.buddha@outlook.sg>
2021-11-14 22:36:05 +0800

    leetcode binary-search-704 update

--- leetcode/easy/binary-search-704/Solution.java
+++ leetcode/easy/binary-search-704/Solution.java
@@ -3,25 +3,32 @@
 Time complexity: O(log N)
 Space complexity: O(1)
 
-Textbook left to right pointer search to middle algo
+left to right bound search for middle element
 
-Criteria: array is sorted
+Criteria: array is sorted 
+(sometimes good to sort and then do multiple short operations like binary search) 
+
+Essentially, by halfing your search limit each iteration, O(log N) is achieved
 
 */
 
 class Solution {
-        public int search(int[] nums, int target) {
-                int left = 0;
-                int right = nums.length-1;
-
-		// necessary to be >= as target could be the exact middle index
-                while (right>=left) { 
-                        if (nums[right] == target) return right;
-                        if (nums[left] == target) return left;
-                        right--;
-                        left++;
-                 
-                }
-                return -1;
+    public int search(int[] nums, int target) {
+        int right = nums.length-1;
+        int left = 0;
+        int mid = (right+left)/2;
+        
+        // necessary to have <= as it might exactly the middle element
+        while (left<=right) {
+            
+            if (nums[mid] < target) left = mid+1; 
+            if (nums[mid] > target) right = mid-1;
+            if (nums[mid] == target) return mid;
+            
+            // update the middle pointer with floor division
+             mid = (left + right)/2;
         }
+        
+        return -1;
+    }
 }

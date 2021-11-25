03c392adacc288adb9cbb4cf6acc7a6e79ef5bc3
Deepak Buddha <deepak.buddha@outlook.sg>
2021-11-25 22:51:19 +0800

    leetcode:easy 21

---
+++ leetcode/easy/two_pointers/merge_two_sorted_lists_21/Solution.java
@@ -0,0 +1,39 @@
+/**
+
+   Time complexity: O(N+M)
+   Space complexity: O(1)
+   
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+                // maintain an unchanging reference to node ahead of the return node.
+        ListNode prehead = new ListNode(-1);
+
+        // key is to use the prev to traverse both lists and return the final answer as the node linked to prev.next
+        ListNode prev = prehead;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+
+        // At least one of l1 and l2 can still have nodes at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = list1 == null ? list2 : list1;
+
+        return prehead.next;
+    }
+}

15c2f33e0f31b19ed1bdccbc01df257f8ef9a611
Deepak Buddha <deepak.buddha@outlook.sg>
2021-11-30 21:09:43 +0800

    leetcode:easy 700

---
+++ leetcode/easy/tree/search_in_a_binary_tree_700/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // whenever a result needs to be recorded, a global variable is necessary
+    TreeNode result = null;
+    
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return root;
+        }
+        
+        searchBST(root.left, val);
+        if (root.val == val) {
+            result= root;
+        }
+        searchBST(root.right, val);
+        
+        return result;
+    }
+}
